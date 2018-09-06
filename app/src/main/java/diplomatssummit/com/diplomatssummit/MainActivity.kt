d88e3b0f38f0f716e62fa3d337bb41020b9b1e6d
merrillkoshy <merrillkoshy@gmail.com>
2018-09-06 17:09:24 +0400

    Jsoup update

--- app/src/main/java/diplomatssummit/com/diplomatssummit/MainActivity.kt
+++ app/src/main/java/diplomatssummit/com/diplomatssummit/MainActivity.kt
@@ -19,7 +19,11 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 import java.io.IOException
-import android.os.AsyncTask
+import java.io.File
+import java.io.FileWriter
+import java.util.Collections.replaceAll
+
+
 
 
 
@@ -48,10 +52,10 @@ class MainActivity : AppCompatActivity() {
       override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_main)
-//          val r =MyTask().execute()
-////        val url = "https://10times.com/dubai-ae"
-////        val doc = Jsoup.connect(url).get()
-////        val title = doc.title()
+//        val r =MyTask().execute()
+//        val url = "https://10times.com/dubai-ae"
+//        val doc = Jsoup.connect(url).get()
+//        val title = doc.title()
 //        val textView: TextView = findViewById(R.id.textView2)
 //        textView.setText("" + r)
 
@@ -60,39 +64,71 @@ class MainActivity : AppCompatActivity() {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
 
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.loadUrl("https://google.com")
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.webViewClient = WebViewClient()
-        (WebViewClient())
+//        val myWebView: WebView = findViewById(R.id.webview)
+//        myWebView.loadUrl("https://google.com")
+//        myWebView.settings.javaScriptEnabled = true
+//        myWebView.webViewClient = WebViewClient()
+//        (WebViewClient())
 
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        val myWebView: WebView = findViewById(R.id.webview)
-        // Check if the key event was the Back button and if there's history
-        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
-            myWebView.goBack()
-            return true
-        }
-        // If it wasn't the Back key or there's no web page history, bubble up to the default
-        // system behavior (probably exit the activity)
-        return super.onKeyDown(keyCode, event)
-    }
+//    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+//        val myWebView: WebView = findViewById(R.id.webview)
+//        // Check if the key event was the Back button and if there's history
+//        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+//            myWebView.goBack()
+//            return true
+//        }
+//        // If it wasn't the Back key or there's no web page history, bubble up to the default
+//        // system behavior (probably exit the activity)
+//        return super.onKeyDown(keyCode, event)
+//    }
 
 
 
     var background = object : Thread(){
         override fun run() {
             val doc: Document
+//            fun WriteToFile(s: String) {
+//                try {
+//                    val fo = FileWriter("/app/src/myfile.html",true)
+//                    fo.write(s)
+//                    fo.close()
+//                }catch (e:IOException){
+//                    e.printStackTrace()
+//                }
+//            }
             try {
                 doc = Jsoup.connect("https://10times.com/dubai-ae").get()
-                val divTag = doc.getElementById("content")
+                val links = doc.select("a[href]")
+                val media = doc.select("[src]")
+                val imports = doc.select("link[href]")
+                for (i in 1..2){
+                    val stringer="#content > tr:nth-child("+i+")"
+                    val selecting=doc.select(stringer)
+                    val s=Jsoup.parse(selecting.toString())
+                    val pattern = "<[^>]*>".toRegex()
 
-                this@MainActivity.runOnUiThread(java.lang.Runnable {
-                    val textView: TextView = findViewById(R.id.textView2)
-                    textView.setText("" + divTag)
-                })
+                    var str = s.toString()
+                    val oldValue = pattern
+                    val newValue = ""
+
+                    val output = str.replace(oldValue, newValue)
+                    val otherStrings = arrayOf(output)
+
+                    this@MainActivity.runOnUiThread(java.lang.Runnable {
+                        val textView: TextView = findViewById(R.id.textView)
+                        textView.setText(""+output)
+                    })
+                }
+//                fun main(args: Array<String>) {
+//                    val outString: String = s.toString()
+//                    File("/app/src/myfile.html").printWriter().use { out -> out.println(outString) }
+//                }
+
+
+
+
 
             } catch (e: IOException) {
                 e.printStackTrace()
